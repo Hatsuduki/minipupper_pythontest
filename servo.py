66469f8a494a59330465a66b4a6c9adcf6ac7cb1
hatsuduki <hatsuduki80@gmail.com>
2022-06-16 12:07:14 +0900

    move one leg

--- servo.py
+++ servo.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
+
 import pi_servo_hat
 import time
+import math
 
 I2C_ADDR = 0x40
 pca9685 = pi_servo_hat.PiServoHat(address=I2C_ADDR)
 pca9685.restart()
 
-def setPosition():
+def initPosition():
     #front-right
     pca9685.move_servo_position(15, 50) #top roll
     pca9685.move_servo_position(14, 45) #middle leg
@@ -26,18 +29,24 @@ def setPosition():
     pca9685.move_servo_position(5, 80) #middle leg
     pca9685.move_servo_position(4, 85) #bottom leg
 
-#setPosition()
 
-#if you want to make legs free
-#pca9685.restart()
+L1 = 50
+L2 = 56
+def servo_FR(x, z):
+    ld = math.sqrt(x*x + z*z)
+    phi = math.atan2(x, z)
 
-pca9685.move_servo_position(15, 50) #top roll
-pca9685.move_servo_position(14, 60) #middle leg
-pca9685.move_servo_position(13, 45) #bottom leg
+    the1 = phi - math.acos((L1*L1 + ld*ld - L2*L2)/(2*L1*ld))
+    the2 = math.acos((ld*ld - L1*L1 - L2*L2)/(2*L1*L2)) - 45*math.pi/180 + the1# -45はL1とL2のなす外側の角 - L1のz軸からの角度の大きさ
 
-#get current position
-print(pca9685.get_servo_position(15))
-print(pca9685.get_servo_position(14))
-print(pca9685.get_servo_position(13))
+    pca9685.move_servo_position(14, the1*180/math.pi + 85)#85はL1が垂直になる角度
+    pca9685.move_servo_position(13, the2*180/math.pi + 45)#45はL2のinitPosition角度
+    #print(the1*180/math.pi + 85)
+    #print(the2*180/math.pi + 45)
 
+initPosition()
 
+time.sleep(1)
+for i in -40, -30, -20, -10, 0, 10, 20, 30, 40, 50:
+    servo_FR(i, 75)
+    time.sleep(0.5)

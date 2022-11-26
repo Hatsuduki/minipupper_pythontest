a8281e03bedeac6ca6ca89a79be2f93d07baa900
Hatsuduki <programming.site.d@gmail.com>
2022-11-26 10:10:17 +0900

    update

--- temp/dependencies.sh
+++ temp/dependencies.sh
@@ -10,6 +10,19 @@ pip3 install adafruit-circuitpython-bno055
 #Doc https://docs.circuitpython.org/projects/bno055/en/latest/#
 
 pip3 install pigpio
-sudo apt install python-pigpio python3-pigpio
+sudo apt install python3-pigpio
+
+
+#< sudo vi /lib/systemd/system/pigpiod.service >
+
+#[Unit]
+#Description=Daemon required to control GPIO pins via pigpio
+#[Service]
+#ExecStart=/usr/local/bin/pigpiod -l
+#ExecStop=/bin/systemctl kill pigpiod
+#Type=forking
+#[Install]
+#WantedBy=multi-user.target
+
 sudo systemctl start pigpiod
 sudo systemctl enable pigpiod

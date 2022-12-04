f0e3b85ee183521330f44b2de7ff8df9a92e98ce
Hatsuduki <programming.site.d@gmail.com>
2022-12-04 11:15:32 +0900

    update

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
 from scan_stairs import scan
-from stabilize_4 import stabilize
+import stabilize_4
 import time
 
-stabilize(sec=3)
+
+leg_point_x, leg_point_z = scan()
+
+print(leg_point_x, leg_point_z)
+
+stabilize_4.servo_FL(0, 40)
+time.sleep(2)
+stabilize_4.servo_FL(45+leg_point_x, 40)
+time.sleep(2)
+stabilize_4.servo_FL(45+leg_point_x, -1*leg_point_z-32)
+time.sleep(1)
+#stabilize(sec=3)
 
 #scan()
 #leg_pointが2つ以上の時に脚を動かし、１つしかないときは少しだけ前後に移動する
--- climbing_stairs/scan_stairs.py
+++ climbing_stairs/scan_stairs.py
@@ -34,8 +34,7 @@ def scan():
     ToF.set_timing_budget_in_ms(30)
     ToF.set_roi(12, 6, 59)
 
-    running = True
-    """
+    
     def exit_handler(signal, frame):
         global running
         running = False
@@ -44,8 +43,8 @@ def scan():
         pca9685.move_servo_position(2, pulse[0])
         print()
         sys.exit(0)
-    """
-    def exit_handler():
+    
+    def finish_handler():
         global running
         running = False
         time.sleep(0.5)
@@ -58,6 +57,7 @@ def scan():
     signal.signal(signal.SIGINT, exit_handler)
 
 
+    running = True
     count = 0
     distance = [] # Initialize list
     three_point = []
@@ -79,105 +79,109 @@ def scan():
     ToF.start_ranging()		
     time.sleep(0.15)
 
-    #while running:
-    for k in theta:
-        #start = time.time()
-        #pi.set_servo_pulsewidth(21, pulse[count])   #set servo
-        pca9685.move_servo_position(2, pulse[count])
+    while running:
+        for k in theta:
+            #start = time.time()
+            #pi.set_servo_pulsewidth(21, pulse[count])   #set servo
+            pca9685.move_servo_position(2, pulse[count])
 
-        try:
-            for i in range(10):
-                time.sleep(0.06)
-                distance.append(ToF.get_distance()+20)  # Get the result of the measurement from the sensor
-            
-        except Exception as e:
-            print(e)
+            try:
+                for i in range(10):
+                    time.sleep(0.06)
+                    distance.append(ToF.get_distance()+20)  # Get the result of the measurement from the sensor
+                
+            except Exception as e:
+                print(e)
 
-        print(distance)
-        avgdistance = statistics.mean(distance) # Running average of last 10 measurements
-        distance.clear()
-        
-        if first_flag == True:
-            x = avgdistance*np.sin(theta[count])
-            z = -1*avgdistance*np.cos(theta[count])
-            first_axis.append(x)
-            first_axis.append(z)
-            first_flag = False
-        else:
-            x = (avgdistance*np.sin(theta[count]) - first_axis[0])*np.cos(rotate_deg*np.pi/180) - (-1*avgdistance*np.cos(theta[count]) - first_axis[1])*np.sin(rotate_deg*np.pi/180) + first_axis[0]
-            z = (avgdistance*np.sin(theta[count]) - first_axis[0])*np.sin(rotate_deg*np.pi/180) + (-1*avgdistance*np.cos(theta[count]) - first_axis[1])*np.cos(rotate_deg*np.pi/180) + first_axis[1]
-        #print(x, z)
-        total_x.append(x)
-        total_z.append(z)
-    
-        #end = time.time()
-        #print("avgDistance(mm): %.2f     deg: %.2f    count: %s    time: %.2f" % (avgdistance, theta[count]*180/np.pi, count, (end-start)))#AAA
-    
-        if 10 < avgdistance and avgdistance < 200:  # distance threshold
-            if line_flag == True:
-                line_x.append(x)# start point
-                line_z.append(z)
-                line_flag = False
-            three_point.append([x, z])
-            if len(three_point) > 3:
-                del three_point[0]
-
-        if len(three_point) >= 3:
-            slope_1 = (three_point[1][1] - three_point[0][1]) / (three_point[1][0] - three_point[0][0]) # average slope of two former
-            slope_2 = (three_point[2][1] - three_point[1][1]) / (three_point[2][0] - three_point[1][0]) # of two latter
-            avg_slope = (slope_1 + slope_2) / 2 # of all
-        
-        if (avg_slope > pre_slope and avg_slope > 1) or (count == len(theta)-1):# 傾きが前のやつより大きくて1以上　または　前のやつより小さくて１以下   
-            if three_point[0][0] - line_x[len(line_x)-1] > 10:
-                pre_z = line_z[len(line_z)-1]
-                line_x.append(three_point[0][0])
-                line_z.append(pre_z)
-        if avg_slope < pre_slope and avg_slope < 1:
-            if three_point[0][1] - line_z[len(line_z)-1] > 10:
-                pre_x = line_x[len(line_x)-1]
-                line_x.append(pre_x)
-                line_z.append(three_point[0][1])
-
-        pre_slope = avg_slope
-
-        count += 1
-
-        if count >= len(theta):
-            line_flag = True
-            first_flag = True
-            leg_point_x = []
-            leg_point_z = []
-            for i in range(len(line_x)):
-                if i%2 == 1:
-                    leg_point_x.append((line_x[i]+line_x[i-1])/2)
-                    leg_point_z.append(line_z[i])
-
-            plt.cla()
-            ax.set_aspect("equal")
-            ax.set_xlabel("x")
-            ax.set_ylabel("z")
-            ax.grid(ls=":")
-            ax.set_xlim([-40, 120])
-            ax.set_ylim([-160, 9])
-            ax.plot(line_x, line_z, marker=".", color="black")
-            #ax.plot(total_x, total_z, marker=".", color="red", linestyle="None")
-            ax.plot(leg_point_x, leg_point_z, marker=".", color="red", linestyle="None")
-
-            pca9685.move_servo_position(2, pulse[0])
-            time.sleep(1)
-
+            print(distance)
+            avgdistance = statistics.mean(distance) # Running average of last 10 measurements
             distance.clear()
-            three_point.clear()
-            line_x.clear()
-            line_z.clear()
-            total_x.clear()
-            total_z.clear()
-            avg_slope = 0
-            pre_slope = 0
-            print()
-            plt.savefig('./img/figure_00.png')
+            
+            if first_flag == True:
+                x = avgdistance*np.sin(theta[count])
+                z = -1*avgdistance*np.cos(theta[count])
+                first_axis.append(x)
+                first_axis.append(z)
+                first_flag = False
+            else:
+                x = (avgdistance*np.sin(theta[count]) - first_axis[0])*np.cos(rotate_deg*np.pi/180) - (-1*avgdistance*np.cos(theta[count]) - first_axis[1])*np.sin(rotate_deg*np.pi/180) + first_axis[0]
+                z = (avgdistance*np.sin(theta[count]) - first_axis[0])*np.sin(rotate_deg*np.pi/180) + (-1*avgdistance*np.cos(theta[count]) - first_axis[1])*np.cos(rotate_deg*np.pi/180) + first_axis[1]
+            #print(x, z)
+            total_x.append(x)
+            total_z.append(z)
+        
+            #end = time.time()
+            #print("avgDistance(mm): %.2f     deg: %.2f    count: %s    time: %.2f" % (avgdistance, theta[count]*180/np.pi, count, (end-start)))#AAA
+        
+            if 10 < avgdistance and avgdistance < 200:  # distance threshold
+                if line_flag == True:
+                    line_x.append(x)# start point
+                    line_z.append(z)
+                    line_flag = False
+                three_point.append([x, z])
+                if len(three_point) > 3:
+                    del three_point[0]
 
-            count = 0
+            if len(three_point) >= 3:
+                slope_1 = (three_point[1][1] - three_point[0][1]) / (three_point[1][0] - three_point[0][0]) # average slope of two former
+                slope_2 = (three_point[2][1] - three_point[1][1]) / (three_point[2][0] - three_point[1][0]) # of two latter
+                avg_slope = (slope_1 + slope_2) / 2 # of all
+            
+            if (avg_slope > pre_slope and avg_slope > 1) or (count == len(theta)-1):# 傾きが前のやつより大きくて1以上　または　前のやつより小さくて１以下   
+                if three_point[0][0] - line_x[len(line_x)-1] > 10:
+                    pre_z = line_z[len(line_z)-1]
+                    line_x.append(three_point[0][0])
+                    line_z.append(pre_z)
+            if avg_slope < pre_slope and avg_slope < 1:
+                if three_point[0][1] - line_z[len(line_z)-1] > 10:
+                    pre_x = line_x[len(line_x)-1]
+                    line_x.append(pre_x)
+                    line_z.append(three_point[0][1])
 
-    exit_handler()
+            pre_slope = avg_slope
+
+            count += 1
+
+            if count >= len(theta):
+                line_flag = True
+                first_flag = True
+                leg_point_x = []
+                leg_point_z = []
+                for i in range(len(line_x)):
+                    if i%2 == 1:
+                        leg_point_x.append((line_x[i]+line_x[i-1])/2)
+                        leg_point_z.append(line_z[i])
+                if len(leg_point_x) >= 2: #pointが角でなかったら,に変更<<================================
+                    running = False
+
+                plt.cla()
+                ax.set_aspect("equal")
+                ax.set_xlabel("x")
+                ax.set_ylabel("z")
+                ax.grid(ls=":")
+                ax.set_xlim([-40, 120])
+                ax.set_ylim([-160, 9])
+                ax.plot(line_x, line_z, marker=".", color="black")
+                #ax.plot(total_x, total_z, marker=".", color="red", linestyle="None")
+                ax.plot(leg_point_x, leg_point_z, marker=".", color="red", linestyle="None")
+
+                pca9685.move_servo_position(2, pulse[0])
+                time.sleep(1)
+
+                distance.clear()
+                three_point.clear()
+                line_x.clear()
+                line_z.clear()
+                total_x.clear()
+                total_z.clear()
+                avg_slope = 0
+                pre_slope = 0
+                print()
+                plt.savefig('./img/figure_00.png')
+
+                count = 0
+
+    finish_handler()
+
+    return leg_point_x[0], leg_point_z[0]
         
